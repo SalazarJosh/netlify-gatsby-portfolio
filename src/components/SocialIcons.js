2301b93fcd7f21984bcc06ded9190a05c55d5ad0
Joshua Salazar <6354268+SalazarJosh@users.noreply.github.com>
2023-04-13 11:24:16 -0400

    removed twitter

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faTwitter} from '@fortawesome/free-brands-svg-icons'
 import {faLinkedin} from '@fortawesome/free-brands-svg-icons'
 import {faCodepen} from '@fortawesome/free-brands-svg-icons'
 import {faGithub} from '@fortawesome/free-brands-svg-icons'
@@ -9,13 +8,6 @@ const SocialIcons = class extends React.Component {
   render() {
     return (
       <>
-        <div className="navbar-item socialIconFooter">
-          <a href="https://twitter.com/josalaza" target="_blank" rel="noopener noreferrer">
-            <span className="icon">
-              <FontAwesomeIcon icon={faTwitter}/>
-            </span>
-          </a>
-        </div>
         <div className="navbar-item socialIconFooter">
           <a href="https://www.linkedin.com/in/joshuasalazar1/" target="_blank" rel="noopener noreferrer">
             <span className="icon">
